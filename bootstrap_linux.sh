ae799e2323896e85894c33c744aef7ec6fc3641f
Christian Roman <ricanontherun@gmail.com>
2025-04-26 12:40:43 -0400

    trim vimrc

--- bootstrap_linux.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/sh
-
-sudo apt update -y
-sudo apt install -y \
-  curl jq neofetch silversearcher-ag zsh
-
-# Install and configure oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
